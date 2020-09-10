2457a38370dd82ee92f936317aff25f6d2ebe2cd
krarumugam <45592342+krarumugam@users.noreply.github.com>
2020-09-10 19:26:24 +0530

    Link added for Trigger

--- scripts/Event_trigger_postgresql.sql
+++ scripts/Event_trigger_postgresql.sql
@@ -1,4 +1,6 @@
 --https://www.postgresql.org/docs/9.4/plpgsql-trigger.html
+--https://www.tutorialdba.com/2017/09/how-to-create-postgresql-plpgsql.html
+
 --DROP EVENT TRIGGER IF EXISTS snitch;
 
 CREATE OR REPLACE FUNCTION snitch() RETURNS event_trigger AS $$
